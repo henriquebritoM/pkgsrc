9076f286b18f6b637cf2dd49d407a92765a75212
henriquebritoM <henrique.britomelo667@gmail.com>
2026-08-30 11:43:14 -0300

    simplified options

--- emulators/compat_linux_test_project/options.mk
+++ emulators/compat_linux_test_project/options.mk
@@ -1,13 +1,12 @@
-PKG_OPTIONS_VAR=                PKG_OPTIONS.compat_linux_test_project
-PKG_OPTIONS_REQUIRED_GROUPS=	link-mode
-PKG_OPTIONS_GROUP.link-mode=	static dynamic
-PKG_SUGGESTED_OPTIONS=			dynamic
+PKG_OPTIONS_VAR=        PKG_OPTIONS.compat_linux_test_project
+PKG_SUPPORTED_OPTIONS=  static
+PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mstatic)
-LINK_MODE=	static
-.elif !empty(PKG_OPTIONS:Mdynamic)
-LINK_MODE=	dynamic
-EMUL_MODULES.linux=	base
+LINK_MODE=      static
+.else
+LINK_MODE=      dynamic
+EMUL_MODULES.linux=     base
 .endif
